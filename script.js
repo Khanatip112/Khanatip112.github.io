e144fe1f5927c7e282cef06f138f6a9f7bdf56ee
Khanatip112 <121042521+Khanatip112@users.noreply.github.com>
2024-10-29 16:24:36 +0700

    Update script.js

--- script.js
+++ script.js
@@ -9,4 +9,19 @@ function scrollToNext() {
     window.scrollBy({
         top: window.innerHeight, // เลื่อนลงไปยังความสูงของหน้าจอ
         behavior: 'smooth' // การเลื่อนแบบนุ่มนวล
-   
+    });
+}
+
+// ฟังก์ชันเพื่อแสดงหรือซ่อนข้อมูลเพิ่มเติมใน About Me
+function toggleAbout() {
+    const moreInfo = document.querySelector(".more-info");
+    const toggleButton = document.querySelector(".expand-toggle");
+
+    if (moreInfo.style.display === "block") {
+        moreInfo.style.display = "none"; // ซ่อน
+        toggleButton.textContent = "+"; // เปลี่ยนเป็นเครื่องหมายบวก
+    } else {
+        moreInfo.style.display = "block"; // แสดง
+        toggleButton.textContent = "-"; // เปลี่ยนเป็นเครื่องหมายลบ
+    }
+}
