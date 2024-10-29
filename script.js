cf88589a644ac128a4262b91f8cc5ca5e81527f6
Khanatip112 <121042521+Khanatip112@users.noreply.github.com>
2024-10-29 15:17:42 +0700

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,20 @@ function toggleMenu() {
     menu.classList.toggle("show");
 }
 
+function toggleAbout() {
+    const moreInfo = document.querySelector(".more-info");
+    const button = document.querySelector(".expand-toggle");
+
+    if (moreInfo.style.display === "none") {
+        moreInfo.style.display = "block";
+        button.textContent = "-"; // เปลี่ยนเป็นเครื่องหมายลบ
+    } else {
+        moreInfo.style.display = "none";
+        button.textContent = "+"; // เปลี่ยนกลับเป็นเครื่องหมายบวก
+    }
+}
+
+
 // ฟังก์ชันเพื่อเลื่อนลงไปยังส่วนถัดไป
 function scrollToNext() {
     window.scrollBy({
